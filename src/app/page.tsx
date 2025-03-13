daf27697c8d760f912f4959bcbf8aa4e5a060d71
futantan <luckytantanfu@gmail.com>
2025-03-13 17:03:52 +1100

    style: ✨ add margin to heading in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Hero } from '@/features/landing-page/hero';
 export default function Home() {
   return (
     <div className='container h-full bg-gray-100 py-10'>
-      <h1 className='text-4xl font-bold'>Hello Next.js playground</h1>
+      <h1 className='mb-4 text-4xl font-bold'>Hello Next.js playground</h1>
       <main className='flex flex-col'>
         <Hero />
       </main>
